7877d76e5854abb1a3b7a2219991787d2abd5966
James <jbyrne5094@gmail.com>
2021-12-24 13:41:12 -0800

    added image to homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,47 +1,15 @@
 import '../Styles/Homepage.css';
-import { useCallback, useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarToggler,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBBtn,
-  MDBDropdown,
-  MDBDropdownToggle,
-  MDBDropdownMenu,
-  MDBDropdownItem,
-  MDBDropdownLink,
   MDBCard, 
   MDBCardBody, 
-  MDBCardTitle, 
-  MDBCardText,
   MDBCardHeader,
-  MDBTypography
 } from 'mdb-react-ui-kit';
 import styled from 'styled-components'
-import axios from 'axios';
+import Navbar from '../Components/Navbar.js'
 
 const PageBackground = styled.section`
   background-color: #F8EDE3;
   height: 100%;
-`;
-
-const NavbarBackground = styled.section`
-  // background: #FFE2C1;
-  background: #F8EDE3;
-`;
-
-const TitleSubtitle = styled.p`
-  color: #000000;
-  text-align: center;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  font-size: 18px; 
 `;
 
 const Section = styled.div`
@@ -52,8 +20,8 @@ const Section = styled.div`
 `
 
 const MainTitleSection = styled.div`
-  margin-left: 10%;
-  margin-right: 10%;
+  margin-left: 15%;
+  margin-right: 15%;
   margin-top: 35px;
   margin-bottom: 35px;
 `
@@ -93,135 +61,85 @@ const CustomListItem = styled.li`
 `;
 
 function Homepage() {
-  const [bars, setBars] = useState('')
-  const [showBasic, setShowBasic] = useState(false);
-
-  const getAllBars = useCallback(async () => {
-    // we will use nginx to redirect it to the proper URL
-    const allBars = await axios.get("/api/");
-    // setBars(allBars.data.rows.map(row => row.number))
-    console.log(allBars)
-  }, []);
-
   return (
-    <PageBackground>
-      <NavbarBackground>
-      <MDBNavbar expand='lg' sticky light className="sticky-top">
-        <MDBContainer fluid>
-          <MDBNavbarBrand href='#'>
-            <MDBTypography tag='div' className='display-6'>
-              Chocolate Insights
-            </MDBTypography>
-          </MDBNavbarBrand>
-            <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-            <MDBNavbarItem>
-              <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link'>
-                  We Are
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem>
-                    <MDBDropdownLink href='who'>Who</MDBDropdownLink>
-                  </MDBDropdownItem>
-                  <MDBDropdownItem>
-                    <MDBDropdownLink href='#what'>What</MDBDropdownLink>
-                  </MDBDropdownItem>
-                  <MDBDropdownItem>
-                    <MDBDropdownLink href='#why'>Why</MDBDropdownLink>
-                  </MDBDropdownItem>
-                  <MDBDropdownItem>
-                    <MDBDropdownLink href='#how'>How</MDBDropdownLink>
-                  </MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' aria-disabled='true'>
-                Chocolate Discovery Map
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' aria-disabled='true'>
-                Craft Bar Database
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            </MDBNavbarNav>
-        </MDBContainer>
-      </MDBNavbar>
-      </NavbarBackground>
-      <MainTitleSection>
-        <MainWebsiteHeading>Welcome to Chocolate Insights!</MainWebsiteHeading>
-        <WebsiteSubheading>
-          Currently under development, <b><i>Chocolate Insights</i></b> will be a service to help pull back the curtain of the <b><i> craft chocolate bean to bar process</i></b> for all chocolate lovers.
-          Learn what we are about below.
-        </WebsiteSubheading>
-      </MainTitleSection>
-      <Section id="who">
-        <SectionHeader>
-          Who
-        </SectionHeader>
-        <MDBCard className="about-card">
-          <MDBCardBody>
-            <SectionContents>
-            We are dedicated team of chocolate enthusiasts seeking fuse our love of <b>craft chocolate</b> with our love of <b>technology and data</b>. 
-          </SectionContents>
-          </MDBCardBody>
-        </MDBCard>
-      </Section>
-      <Section id="what">
-        <SectionHeader>
-         What
-        </SectionHeader>
-        <MDBCard className="about-card">
-          <MDBCardBody>
-            <SectionContents>
-              <b>Chocolate Insights</b> is a service dedicated to enhancing the chocolate buying experience by <b>erasing the information barrier</b> between choco lovers and their bars.
-              Our service will allow <b>interactive visualizations</b> of bean to bar data to be presented to <b>customers' phones</b> while browsing in-store via <b>QR code</b> tehcnology.
+    <body>
+      <PageBackground className='full-page-image-bg'>
+        <Navbar />
+        <MainTitleSection>
+          {/* <MainWebsiteHeading>Welcome to Chocolate Insights!</MainWebsiteHeading> */}
+          <WebsiteSubheading>
+            Currently under development, <b><i>Chocolate Insights</i></b> will be a service to help pull back the curtain of the <b><i> craft chocolate bean to bar process</i></b> for all chocolate lovers.
+          </WebsiteSubheading>
+          <WebsiteSubheading>
+            Learn what we are about below.
+          </WebsiteSubheading>
+        </MainTitleSection>
+        <Section id="who">
+          <SectionHeader>
+            Who
+          </SectionHeader>
+          <MDBCard className="about-card">
+            <MDBCardBody>
+              <SectionContents>
+              We are dedicated team of chocolate enthusiasts seeking fuse our love of <b>craft chocolate</b> with our love of <b>technology and data</b>. 
             </SectionContents>
-          </MDBCardBody>
-        </MDBCard>
-      </Section>
-      <Section id="why">
-        <SectionHeader>
-         Why
-        </SectionHeader>
-        <MDBCard className="about-card">
-          <MDBCardBody>
-            <SectionContents>
-              <ol class="chocolate-list">
-                <CustomListItem>Help <b>customers</b> learn about chocolate</CustomListItem>
-                <CustomListItem>Help <b>sellers</b> increase bar purchases</CustomListItem>
-                <CustomListItem>Help <b>makers</b> tell their stories</CustomListItem>
-                <CustomListItem>Help <b>growers</b> and <b>producers</b> get exposure</CustomListItem>
-              </ol>
-            </SectionContents>
-          </MDBCardBody>
-        </MDBCard>
-      </Section>
-      <Section id="how">
-        <SectionHeader>
-         How
-        </SectionHeader>
-        <MDBCard className="about-card">
-          <MDBCardBody>
-            {/* <SectionContents> */}
-              <MDBCardHeader className="about-card-header">
-                How to use our service for your <b>bean to bar visualizations</b>
-              </MDBCardHeader>
-            {/* </SectionContents> */}
-            <SectionContents>
-              <ol>
-                <CustomListItem><b>Find</b> your bar in the Chocolate Insights Database</CustomListItem>
-                <CustomListItem><b>Customize</b> your bean to bar visualization</CustomListItem>
-                <CustomListItem><b>Receive</b> the QR code associated with your visual</CustomListItem>
-                <CustomListItem><b>Print</b> out the QR code and <b>put</b> it near your bar in store</CustomListItem>
-                <CustomListItem><b>Watch</b> as your customers' chocolate knowledge, and sales, <b>increase</b>!</CustomListItem>
-              </ol>
-            </SectionContents>
-          </MDBCardBody>
-        </MDBCard>
-      </Section>
-    </PageBackground>
+            </MDBCardBody>
+          </MDBCard>
+        </Section>
+        <Section id="what">
+          <SectionHeader>
+          What
+          </SectionHeader>
+          <MDBCard className="about-card">
+            <MDBCardBody>
+              <SectionContents>
+                <b>Chocolate Insights</b> is a service dedicated to enhancing the chocolate buying experience by <b>erasing the information barrier</b> between choco lovers and their bars.
+                Our service will allow <b>interactive visualizations</b> of bean to bar data to be presented to <b>customers' phones</b> while browsing in-store via <b>QR code</b> tehcnology.
+              </SectionContents>
+            </MDBCardBody>
+          </MDBCard>
+        </Section>
+        <Section id="why">
+          <SectionHeader>
+          Why
+          </SectionHeader>
+          <MDBCard className="about-card">
+            <MDBCardBody>
+              <SectionContents>
+                <ol class="chocolate-list">
+                  <CustomListItem>Help <b>customers</b> learn about chocolate</CustomListItem>
+                  <CustomListItem>Help <b>sellers</b> increase bar purchases</CustomListItem>
+                  <CustomListItem>Help <b>makers</b> tell their stories</CustomListItem>
+                  <CustomListItem>Help <b>growers</b> and <b>producers</b> get exposure</CustomListItem>
+                </ol>
+              </SectionContents>
+            </MDBCardBody>
+          </MDBCard>
+        </Section>
+        <Section id="how">
+          <SectionHeader>
+          How
+          </SectionHeader>
+          <MDBCard className="about-card">
+            <MDBCardBody>
+                <MDBCardHeader className="about-card-header">
+                  How to use our service for your <b>bean to bar visualizations</b>
+                </MDBCardHeader>
+              <SectionContents>
+                <ol>
+                  <CustomListItem><b>Find</b> your bar in the Chocolate Insights Database</CustomListItem>
+                  <CustomListItem><b>Customize</b> your bean to bar visualization</CustomListItem>
+                  <CustomListItem><b>Receive</b> the QR code associated with your visual</CustomListItem>
+                  <CustomListItem><b>Print</b> out the QR code and <b>put</b> it near your bar in store</CustomListItem>
+                  <CustomListItem><b>Watch</b> as your customers' chocolate knowledge, and sales, <b>increase</b>!</CustomListItem>
+                </ol>
+              </SectionContents>
+            </MDBCardBody>
+          </MDBCard>
+        </Section>
+      </PageBackground>
+      <PageBackground className="full-page-background"></PageBackground>
+    </body>
   );
 }
 
